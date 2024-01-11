bb31a2f44a8977cf7023bbfe96040711538d68f3
Michael DeSanty <michael.desanty@gmail.com>
2024-01-10 19:57:53 -0500

    Create app template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <Container>
+      <h1 className="mt-3">Replace me with your own stuff!</h1>
+    </Container>
+  );
+}
+
+export default App;
